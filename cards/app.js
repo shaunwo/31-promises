46a14321d9a5408837b10b0bf7e26c9d5255442b
shaunwo <shaunwo@gmail.com>
2022-07-21 19:52:20 -0400

    Completed Step 1 of the exercise

--- cards/app.js
+++ cards/app.js
@@ -1,13 +1,23 @@
 let cardsURL = 'http://deckofcardsapi.com/api/deck/';
 let outputArea = document.getElementById('output');
 
+const capitalize = (str) => {
+  if (typeof str === 'string') {
+    return str.replace(/^\w/, (c) => c.toUpperCase());
+  } else {
+    return '';
+  }
+};
+
 // Step 1
 axios
   .get(`${cardsURL}new/draw/`)
   .then((data) => {
     //console.log(data);
     let { suit, value } = data.data.cards[0];
-    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    console.log(
+      `${capitalize(value.toLowerCase())} of ${capitalize(suit.toLowerCase())}`
+    );
   })
   .catch((err) => console.log(err));
 
