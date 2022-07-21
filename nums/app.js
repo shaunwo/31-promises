e3a37ef745990ae8a512f55b5775e30da32dff26
shaunwo <shaunwo@gmail.com>
2022-07-21 19:13:52 -0400

    Completed Step 3 of the exercise

--- nums/app.js
+++ nums/app.js
@@ -1,12 +1,32 @@
 let numbersURL = 'http://numbersapi.com';
-let randomNum = 7;
+let outputArea = document.getElementById('output');
 
 // Step 1
+let randomNum = 7;
 axios
   .get(`${numbersURL}/${randomNum}?json`)
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
 
 // Step 2
+let multiNums = [11, 45, 15];
+axios
+  .get(`${numbersURL}/${multiNums}?json`)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
 // Step 3
+let fourPromises = [];
+outputArea.innerHTML = `<h2>Step 3</h2>`;
+for (let i = 0; i < 4; i++) {
+  fourPromises.push(axios.get(`${numbersURL}/random?json`));
+}
+Promise.all(fourPromises)
+  .then((numsArr) =>
+    numsArr.forEach(
+      (data) =>
+        //console.log(data.data.text)
+        (outputArea.innerHTML += `  <li>${data.data.text}</li>`)
+    )
+  )
+  .catch((err) => console.log(err));
